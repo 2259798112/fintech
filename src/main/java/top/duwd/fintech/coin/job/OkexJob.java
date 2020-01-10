876d5da78fd97f22a768c374367e70ae1e3ec078
du-wd <du.wd@outlook.com>
2020-01-10 16:18:03 +0800

    2bcheck

--- src/main/java/top/duwd/fintech/coin/job/OkexJob.java
+++ src/main/java/top/duwd/fintech/coin/job/OkexJob.java
@@ -3,9 +3,12 @@ package top.duwd.fintech.coin.job;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import top.duwd.dutil.coin.okex.OkexApiUtil;
+import top.duwd.dutil.common.indicator.I2B;
 import top.duwd.dutil.common.model.CandleModel;
 import top.duwd.fintech.coin.service.OkexKdjCoinService;
 import top.duwd.fintech.common.msg.wx.qiye.WxService;
@@ -17,6 +20,9 @@ import java.util.Map;
 @Component
 @Slf4j
 public class OkexJob {
+    @Autowired
+    private OkexApiUtil okexApiUtil;
+
     @Autowired
     private OkexKdjCoinService okexKdjCoinService;
     private String END = "-USD-200327";
@@ -36,7 +42,7 @@ public class OkexJob {
         2019-12-26 12:59:50
      */
     //15min
-    @Scheduled(cron = "55 4,9,14,19,24,29,34,39,44,49,54,59 * * * ?")
+    //@Scheduled(cron = "55 4,9,14,19,24,29,34,39,44,49,54,59 * * * ?")
     public void run5m() {
         log.info("5m run start");
         int limit = 100;//一天
@@ -52,7 +58,7 @@ public class OkexJob {
         2019-12-26 12:59:50
      */
     //15min
-    @Scheduled(cron = "50 14,29,44,59 * * * ?")
+    //@Scheduled(cron = "50 14,29,44,59 * * * ?")
     public void run15m() {
         log.info("15m run start");
         int limit = 100;//一天
@@ -67,7 +73,7 @@ public class OkexJob {
         2019-12-27 11:59:50
      */
     //4h
-    @Scheduled(cron = "50 59 3,7,11,15,19,23 * * ?")
+    //@Scheduled(cron = "50 59 3,7,11,15,19,23 * * ?")
     public void run4h() {
         log.info("4h run start");
         int limit = 100;//一天
@@ -82,7 +88,7 @@ public class OkexJob {
         2019-12-28 23:59:50
      */
     //1d
-    @Scheduled(cron = "50 59 7,19 * * ?")
+    //@Scheduled(cron = "50 59 7,19 * * ?")
     public void run1d() {
         log.info("1d run start");
         int limit = 100;//一天
@@ -92,7 +98,7 @@ public class OkexJob {
     }
 
     //1d
-    @Scheduled(cron = "50 59 7,19 * * ?")
+    //@Scheduled(cron = "50 59 7,19 * * ?")
     public void run3d() {
         log.info("3d run start");
         int limit = 100;//一天
@@ -117,6 +123,70 @@ public class OkexJob {
             log.info(content);
             wxService.sendText(content);
         }
+    }
+
+    public boolean check2b(String symbol, int period, int limit) {
+        List<CandleModel> candleList = okexApiUtil.getCandleList(symbol, String.valueOf(period * 60), null, null);
+        return I2B.b2(candleList, limit);
+    }
+
+    /*
+    0 1-5 * * * ?
+    2020-01-10 17:01:00
+    2020-01-10 17:02:00
+    2020-01-10 17:03:00
+    2020-01-10 17:04:00
+    2020-01-10 17:05:00
+    2020-01-10 18:01:00
+    2020-01-10 18:02:00
+     */
+    @Scheduled(cron = "0 1-5 * * * ?")
+    @Scheduled(cron = "0 1-5 * * * ?")
+    public void check1h() {
+        String content = BTC + "- 1h 2b -" + getTime();
+        log.info(content);
+        boolean b = this.check2b(BTC, 60, 48);
+        if (b) {
+            log.info(content + " [success]");
+            wxService.sendText(content);
+        }
+    }
+
+    /*
+    2020-01-10 18:01:00
+    2020-01-10 18:02:00
+    2020-01-10 18:03:00
+    2020-01-10 18:04:00
+    2020-01-10 18:05:00
+    2020-01-10 20:01:00
+    2020-01-10 20:02:00
+     */
+    @Scheduled(cron = "0 1-10 */2 * * ?")
+    public void check2h() {
+        String content = BTC + "- 2h 2b -" + getTime();
+        log.info(content);
+        boolean b = this.check2b(BTC, 60 * 2, 36);
+        if (b) {
+            log.info(content + " [success]");
+            wxService.sendText(content);
+        }
+    }
+
+    @Scheduled(cron = "0 1-15 */4 * * ?")
+    public void check4h() {
+        String content = BTC + "- 4h 2b -" + getTime();
+        log.info(content);
+
+        boolean b = this.check2b(BTC, 60 * 4, 24);
+        if (b) {
+            log.info(content + " [success]");
+            wxService.sendText(content);
+        }
+    }
+
+    public static String getTime() {
+        DateTime now = DateTime.now();
+        return now.getYear() + "-" + now.getMonthOfYear() + "-" + now.getDayOfYear() + " " + now.getHourOfDay();
     }
 
 }
