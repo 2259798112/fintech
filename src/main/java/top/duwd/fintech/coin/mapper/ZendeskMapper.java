d1764f7c5f035a2e106519d1641f454720efa9c5
du-wd <du.wd@outlook.com>
2020-01-19 16:43:06 +0800

    check detail

---
+++ src/main/java/top/duwd/fintech/coin/mapper/ZendeskMapper.java
@@ -0,0 +1,7 @@
+package top.duwd.fintech.coin.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import top.duwd.fintech.coin.domain.entity.ZendeskEntity;
+
+public interface ZendeskMapper extends Mapper<ZendeskEntity> {
+}
