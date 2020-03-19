bdb2d6deb6e0f915bb7b41d52247a0c6b0534144
2259798112@qq.com <du.wd@outlook.com>
2020-03-19 17:59:32 +0800

    zhihu

---
+++ src/main/java/top/duwd/fintech/common/mapper/zhihu/ZhihuAnswerMapper.java
@@ -0,0 +1,7 @@
+package top.duwd.fintech.common.mapper.zhihu;
+
+import tk.mybatis.mapper.common.Mapper;
+import top.duwd.fintech.sc.zhihu.model.entity.ZhihuAnswerEntity;
+
+public interface ZhihuAnswerMapper extends Mapper<ZhihuAnswerEntity> {
+}
---
+++ src/main/java/top/duwd/fintech/common/mapper/zhihu/ZhihuQuestionMapper.java
@@ -0,0 +1,7 @@
+package top.duwd.fintech.common.mapper.zhihu;
+
+import tk.mybatis.mapper.common.Mapper;
+import top.duwd.fintech.sc.zhihu.model.entity.ZhihuQuestionEntity;
+
+public interface ZhihuQuestionMapper extends Mapper<ZhihuQuestionEntity> {
+}
